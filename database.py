2f0170b1e81fd6eb388aa55519869b8725c54870
David Rosenstein <drosenstein@wpi.edu>
2022-01-15 11:50:55 -0500

    creating database object and functions

--- database.py
+++ database.py
@@ -1,18 +1,41 @@
 import mysql.connector
 
-db = mysql.connector.connect(
-    host='localhost',
-    user='python',
-    passwd='!nnov4tion',
-    database='questiongame'
-)
 
-cursorObject = db.cursor()
+def opendb(name):
+    if name == "questiongame" or name == "main":
+        return Database("questiongame")
+    return None
 
-cursorObject.execute("SELECT * FROM users");
-results = cursorObject.fetchall();
 
-for x in results:
-    print(x)
+class Database:
 
-db.close()
+    def __init__(self, name):
+        self.db = mysql.connector.connect(
+            host='localhost',
+            user='python',
+            passwd='!nnov4tion',
+            database=name
+        )
+        self.cursorObject = self.db.cursor()
+        self.open = True
+
+    def close(self):
+        self.db.close()
+        self.open = False
+
+    def select(self, rownames, tablename):
+        if not self.open:
+            raise Exception("database is not open!")
+
+        rowstr = ""
+
+        if type(rownames) is str:
+            rowstr = rownames
+        else:
+            for row in rownames:
+                rowstr += row
+                rowstr += ", "
+            rowstr = rowstr[:-2]
+
+        self.cursorObject.execute("SELECT " + rowstr + " FROM " + tablename)
+        return self.cursorObject.fetchall()
