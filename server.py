a3b00225168f85723158061e10d9e90c28b41269
Iv <aerobinsoniv@gmail.com>
2022-01-15 11:04:07 -0500

    Receives API requests

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
+from urllib import request
 from flask import Flask
 from flask import render_template
 from flask import send_from_directory
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,6 +12,14 @@ def index():
 @app.route("/api-test")
 def api_test():
     return render_template("api-test.html")
+
+
+# curl -d '{"key1":"value1", "key2":"value2"}' -H "Content-Type: application/json" -X POST localhost/api/mom
+@app.route("/api/mom", methods=['POST'])
+def api_mom():
+    print("JSON?" + str(request.is_json))
+    print(request.get_json())
+    return "epic poggers\n"
 
 # This part hosts the whole JS folder or something
 # https://stackoverflow.com/questions/20646822/how-to-serve-static-files-in-flask
