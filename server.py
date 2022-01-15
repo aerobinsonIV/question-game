384d098630eccc2e88170a99302ad13a340deb50
Iv <aerobinsoniv@gmail.com>
2022-01-15 10:22:15 -0500

    Made page to start testing and building api

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask import render_template
 from flask import send_from_directory
-
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/api-test")
+def api_test():
+    return render_template("api-test.html")
 
 # This part hosts the whole JS folder or something
 # https://stackoverflow.com/questions/20646822/how-to-serve-static-files-in-flask
