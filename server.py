3fd96f0cd22f56bf6552034b408c90cec89b0ef8
Iv <aerobinsoniv@gmail.com>
2022-01-15 11:11:58 -0500

    Takes in json POST data and responds

--- server.py
+++ server.py
@@ -14,12 +14,11 @@ def api_test():
     return render_template("api-test.html")
 
 
-# curl -d '{"key1":"value1", "key2":"value2"}' -H "Content-Type: application/json" -X POST localhost/api/mom
+# curl -d '{"text":"hello"}' -H "Content-Type: application/json" -X POST localhost/api/mom
 @app.route("/api/mom", methods=['POST'])
-def api_mom():
-    print("JSON?" + str(request.is_json))
-    print(request.get_json())
-    return "epic poggers\n"
+def api_mom():    
+    json = request.get_json()
+    return json['text'] + " you're mom \n"
 
 # This part hosts the whole JS folder or something
 # https://stackoverflow.com/questions/20646822/how-to-serve-static-files-in-flask
