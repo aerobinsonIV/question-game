369df34a654583c94a19a2128dcffaba7038f27a
Iv <aerobinsoniv@gmail.com>
2022-01-15 12:31:39 -0500

    more comments

--- server.py
+++ server.py
@@ -8,7 +8,8 @@ app = flask.Flask(__name__)
 # For all API paths, JSON parsing should happen in this script
 # All API functions should take in one parameter which is a dictionary
 
-# API paths:
+# Page routes:
+
 @app.route("/")
 def index():
     return flask.render_template("index.html")
@@ -16,6 +17,8 @@ def index():
 @app.route("/api-test")
 def api_test():
     return flask.render_template("api-test.html")
+
+# API routes:
 
 # curl -d '{"text":"hello"}' -H "Content-Type: application/json" -X POST localhost/api/mom
 @app.route("/api/mom", methods=['POST'])
