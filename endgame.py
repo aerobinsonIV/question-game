82b422988da947811ba18bc19933164e3509e2ee
Iv <aerobinsoniv@gmail.com>
2022-01-16 11:19:44 -0500

    im trying

--- endgame.py
+++ endgame.py
@@ -1,10 +1,10 @@
 import flask
-uids_done = [-1, -1]
+uids_done = []
 
 def other_player_finished_ping(uid, game_id):
     otherFinished = 0
 
-    if(uids_done[0] != -1 and uids_done[1] != -1):
+    if(len(uids_done) == 2):
         otherFinished = 1
     
     response = {
@@ -16,12 +16,7 @@ def other_player_finished_ping(uid, game_id):
 def accept_answers(uid, game_id, request_json):
     print(f"Got answers from uid {uid}, game id {game_id}")
     
-    if(uids_done[0] == -1):
-        uids_done[0] == uid
-        print(f"here1, set uids_done to {str(uids_done)}")
-    else:
-        uids_done[1] == uid
-        print(f"here2, set uids_done to {str(uids_done)}")
+    uids_done.append(uid)
 
     print(f"set uids_done to {str(uids_done)}")
 
