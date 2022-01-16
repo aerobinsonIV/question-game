930b3adfd0c7da1fdacae423621637bf9cabcb15
Iv <aerobinsoniv@gmail.com>
2022-01-16 00:54:40 -0500

    fix crash on matchmaking not logged by adding error handling for no uid

--- util.py
+++ util.py
@@ -1,10 +1,11 @@
 import database
 
 def get_username(uid):
-    db = database.opendb("main")
-    results = db.select("users", "username", "id=\"" + uid + "\"")
-    db.close()
-    return results[0][0]
+    if uid:
+        db = database.opendb("main")
+        results = db.select("users", "username", "id=\"" + uid + "\"")
+        db.close()
+        return results[0][0]
 
 def check_cookie(uid):
     if uid is None:
