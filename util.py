2649fc8311de040a402b46c919c8cdf4dac66afe
Iv <aerobinsoniv@gmail.com>
2022-01-15 20:32:08 -0500

    proper db column

--- util.py
+++ util.py
@@ -2,6 +2,6 @@ import database
 
 def get_username(uid):
     db = database.opendb("main")
-    results = db.select("users", "*", "user_id=\"" + uid + "\"")
-    db.close
-    return results
+    results = db.select("users", "username", "id=\"" + uid + "\"")
+    db.close()
+    return results[0][0]
