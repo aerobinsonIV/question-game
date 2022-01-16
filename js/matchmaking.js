b9749deae25facd78217830acea306c6e139aba3
Iv <aerobinsoniv@gmail.com>
2022-01-16 04:22:45 -0500

    This seems to work, let me remove the old one

--- js/matchmaking.js
+++ js/matchmaking.js
@@ -5,7 +5,6 @@ function sendMatchmakingPingsUntilMatchFound(){
         if(response.ready === 1){
             console.log("Match found with " + response.partner)
             document.cookie = "game_id=" + response.gameid + "; path=/";
-            window.location.replace("/questions");
             window.location.href = "/questions";
         }else{
         //Rerun this function and ping again asking for a match
