bb65110bcac8790960d985fffb261c6e917d1fcf
Iv <aerobinsoniv@gmail.com>
2022-01-16 09:10:03 -0500

    debugging matchmaking

--- js/matchmaking.js
+++ js/matchmaking.js
@@ -5,7 +5,7 @@ function sendMatchmakingPingsUntilMatchFound(){
         if(response.ready === 1){
             console.log("Match found with " + response.partner)
             document.cookie = "game_id=" + response.game_id + "; path=/";
-            window.location.href = "/questions";
+            // window.location.href = "/questions";
         }else{
         //Rerun this function and ping again asking for a match
         setTimeout(() => {
