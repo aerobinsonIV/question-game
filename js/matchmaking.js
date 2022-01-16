f5644f615e58b1b30c5a923de2172cac3bcfb678
Iv <aerobinsoniv@gmail.com>
2022-01-16 04:19:41 -0500

    redirect to questions page on match start

--- js/matchmaking.js
+++ js/matchmaking.js
@@ -6,6 +6,7 @@ function sendMatchmakingPingsUntilMatchFound(){
         if(response.ready === 1){
             console.log("Match found with " + response.partner)
             document.cookie = "game_id=" + response.gameid + "; path=/";
+            window.location.replace("/questions");
         }else{
         //Rerun this function and ping again asking for a match
         setTimeout(() => {
