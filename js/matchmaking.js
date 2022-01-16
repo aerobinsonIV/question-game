a6bc1957642f7a209a600768a6d00eba3cd1b337
Iv <aerobinsoniv@gmail.com>
2022-01-16 04:22:00 -0500

    trying another method of doing the redirect

--- js/matchmaking.js
+++ js/matchmaking.js
@@ -1,12 +1,12 @@
 function sendMatchmakingPingsUntilMatchFound(){
     
     matchmakingPing().then((response) => {
-        console.log(response);
 
         if(response.ready === 1){
             console.log("Match found with " + response.partner)
             document.cookie = "game_id=" + response.gameid + "; path=/";
             window.location.replace("/questions");
+            window.location.href = "/questions";
         }else{
         //Rerun this function and ping again asking for a match
         setTimeout(() => {
