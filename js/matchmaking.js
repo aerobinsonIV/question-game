76668b2b839c9774ce4823859c7d2142b8ae3b16
Iv <aerobinsoniv@gmail.com>
2022-01-16 01:18:50 -0500

    Add repeated pinging to matchmaking page

--- js/matchmaking.js
+++ js/matchmaking.js
@@ -2,11 +2,18 @@ function sendMatchmakingPingsUntilMatchFound(){
     console.log("In matchmaking.js");
     matchmakingPing().then((response) => {
         console.log(response);
+
+        if(response.ready === "1"){
+            console.log("Match found with uid " + response.other-player-uid)
+        }else{
+        //Rerun this function and ping again asking for a match
+        setTimeout(() => {
+            sendMatchmakingPingsUntilMatchFound();
+        }, 1000);
+        }
     })
 }
 
-sendMatchmakingPingsUntilMatchFound();
-
-// setTimeout(() => {
-//     console.log("this is the second message")
-// }, 3000);
+setTimeout(() => {
+    sendMatchmakingPingsUntilMatchFound();
+}, 1000);
